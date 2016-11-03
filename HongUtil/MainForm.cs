48fc5b1f61469a38e2780857b2ed46df16fea7e5
hongplee-NB\hongplee <hongplee@gmail.com>
2016-11-03 23:44:40 +0900

    초기 파일

---
+++ HongUtil/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * SharpDevelop으로 작성되었습니다.
+ * 사용자: hongplee
+ * 날짜: 2016-11-03
+ * 시간: 오후 11:42
+ * 
+ * 이 템플리트를 변경하려면 [도구->옵션->코드 작성->표준 헤더 편집]을 이용하십시오.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HongUtil
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ HongUtil/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * SharpDevelop으로 작성되었습니다.
+ * 사용자: hongplee
+ * 날짜: 2016-11-03
+ * 시간: 오후 11:42
+ * 
+ * 이 템플리트를 변경하려면 [도구->옵션->코드 작성->표준 헤더 편집]을 이용하십시오.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace HongUtil
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
